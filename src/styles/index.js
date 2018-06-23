85951676162175310f4a0b9bf05531c3426224ec
theJian <thejianmail@gmail.com>
2018-06-23 14:21:17 +0800

    Add font related styles

---
+++ src/styles/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const fontSizes = {
+  xlarge: '46px',
+  large: '24px',
+};
+
+export const styles = StyleSheet.create({
+  eventTitle: {
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
+});
+
+export default styles;
