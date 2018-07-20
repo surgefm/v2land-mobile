c693ec2b380061994528913cdaa39bc6b7c9c4a6
Link <simon.zehua.li@gmail.com>
2018-07-20 07:26:38 -0400

    实现首页顶部图标显示

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,7 +3,9 @@ import { StyleSheet } from 'react-native';
 export const colors = {
   shadow: '#EAEAEA',
   blue: '#4A90E2',
+  grey: '#AEAEAE',
   white: '#FFFFFF',
+  lightGrey: '#F7F7F7',
 }
 
 export const fontSizes = {
@@ -17,5 +19,4 @@ export const styles = StyleSheet.create({
     fontSize: 24,
   }
 });
-
 export default styles;
