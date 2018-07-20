96405da852b4bbc575fcbac58ac9a3b6f0797e85
Link <simon.zehua.li@gmail.com>
2018-07-20 00:18:58 -0400

    实现事件题图的显示

--- src/util/getAssetURL.js
+++ src/util/getAssetURL.js
@@ -1,4 +1,11 @@
 // TODO: normalize asset address
-const getAssetURL = relativePath => relativePath;
+import config from '../config/const';
+
+const getAssetURL = (relativePath, { width, height } = {}) => {
+  if (width && height) {
+    return `${config.cdn}${width}x${height}/${relativePath}`;
+  }
+  return config.cdn + relativePath;
+};
 
 export default getAssetURL;
