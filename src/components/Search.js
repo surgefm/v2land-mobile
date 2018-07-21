36865133a21fe907095b60584530656ab7dab39f
Link <simon.zehua.li@gmail.com>
2018-07-20 20:26:51 -0400

    初步实现 Search 界面

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import { Title } from './Common';
+import { pageStyle, paddings, searchBarStyle } from '../styles';
 
 const Search = () => (
-  <Text>Search</Text>
-)
+  <ScrollView style={pageStyle.default}>
+    <View style={paddings.interval}>
+      <Title>搜索</Title>
+    </View>
+    <SearchBar
+      lightTheme
+      round={true}
+      containerStyle={searchBarStyle.searchContainer}
+      inputStyle={searchBarStyle.searchInput}
+      placeholder='输入事件/新闻关键词'
+    />
+  </ScrollView>
+);
 
 export default Search;
