c7a0c4950e92b271441d49dfcb7e941fd31b8ce6
theJian <thejianmail@gmail.com>
2018-08-04 10:37:34 +0800

    Add service

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,8 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose } from 'redux';
+import { install } from 'redux-loop';
 import reducers from '../reducers';
 
 const configStore = () => {
-  return createStore(reducers /* applyMiddleware() */);
+  return createStore(
+    reducers, // reducers
+    install() // enhancers
+  );
 };
 
 export default configStore;
