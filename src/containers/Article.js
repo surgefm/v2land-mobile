10de35d3b727501cc9d69fc826d86d81785d539e
IncredibLink <simon.zehua.li@gmail.com>
2018-11-12 22:06:14 -0500

    实现 Article Container

--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,10 +1,38 @@
 import R from 'ramda';
+import React from 'react';
 import ArticleComponent from '../components/Article';
-import withNavigationOptions from '../enhancers/withNavigationOptions';
+import { BackButton } from '../components/elements';
+
+import {
+  withNavigationOptions,
+  withNavigationHandlers,
+  connect,
+  prepare,
+} from '../enhancers';
+
+import { fetchEvent } from '../store/actions/events.js';
+import { eventSelector } from '../store/selectors/events.js';
 
 const Article = R.compose(
-  withNavigationOptions({
+  withNavigationOptions(({ navigation }) => ({
     headerTransparent: true,
+    tintColor: 'transparent',
+    headerLeft: (
+      <BackButton onPress={() => navigation.goBack()} />
+    ),
+  })),
+  withNavigationHandlers(({ state, goBack }) => {
+    return {
+      eventId: state.params.eventId,
+    };
+  }),
+  connect({
+    event: eventSelector,
+  }, {
+    fetchEvent,
+  }),
+  prepare(({ fetchEvent, eventId }) => {
+    // fetchEvent({ eventId });
   }),
 )(ArticleComponent);
 
--- src/store/selectors/events.js
+++ src/store/selectors/events.js
@@ -5,3 +5,11 @@ export const eventListSelector = [
   events => events.data,
   data => data || [],
 ];
+
+export const eventSelector = [
+  (state, props) => {
+    if (!state.events || !state.events.data) return [];
+    return state.events.data.filter(e => e.id === props.eventId);
+  },
+  event => event.length ? event[0] : null,
+];
