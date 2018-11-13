2452e964d2ba7edc6a3b9aefa43ee044272ce242
IncredibLink <simon.zehua.li@gmail.com>
2018-11-13 13:59:22 -0500

    Lint the code.

--- src/components/profile/LoginInput.js
+++ src/components/profile/LoginInput.js
@@ -1,63 +1,54 @@
 import React, { Component } from 'react';
 import {
-  ToolbarAndroid,
-  AppRegistry,
   StyleSheet,
-  Text,
   View,
-  Image,
   TextInput,
-  TouchableOpacity,
-  Dimensions
+  Dimensions,
 } from 'react-native';
 
-
-const {height,width} = Dimensions.get('window');
-let widthOfMargin = Dimensions.get('window').width*0.85
-
+const widthOfMargin = Dimensions.get('window').width * 0.85;
 
 export default class EditView extends Component {
   constructor(props) {
-   super(props);
-   this.state = {text: ''};
- }
+    super(props);
+    this.state = { text: '' };
+  }
 
   render() {
     return (
       <View style={LoginStyles.TextInputView}>
-       <TextInput style={LoginStyles.TextInput}
-         placeholder={this.props.name}
-         onChangeText={
-           (text) => {
-             this.setState({text});
-             this.props.onChangeText(text);
-           }
-        }
-       />
-       </View>
+        <TextInput style={LoginStyles.TextInput}
+          placeholder={this.props.name}
+          onChangeText={
+            (text) => {
+              this.setState({ text });
+              this.props.onChangeText(text);
+            }
+          }
+        />
+      </View>
     );
   }
 }
 
-
 const LoginStyles = StyleSheet.create({
   TextInputView: {
     marginTop: 0,
-    height:50,
-    width:widthOfMargin,
+    height: 50,
+    width: widthOfMargin,
     backgroundColor: '#ffffff',
-    borderColor:'#000000',
+    borderColor: '#000000',
     flexDirection: 'column',
-    justifyContent: 'center', 
-    borderTopWidth:0,
-    borderBottomWidth:1,
-    borderLeftWidth:0,
-    borderRightWidth:0,
+    justifyContent: 'center',
+    borderTopWidth: 0,
+    borderBottomWidth: 1,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
   },
 
   TextInput: {
     backgroundColor: '#ffffff',
-    height:45,
-    margin:18,
+    height: 45,
+    margin: 18,
   },
 });
--- src/components/profile/WeiboButton.js
+++ src/components/profile/WeiboButton.js
@@ -1,72 +1,28 @@
 import React, { Component } from 'react';
 import {
-  ToolbarAndroid,
-  AppRegistry,
-  StyleSheet,
-  Text,
   View,
   Image,
-  TextInput,
   TouchableOpacity,
   Dimensions,
-  resizeMode,
-  Alert
+  Alert,
 } from 'react-native';
 
-const {height,width} = Dimensions.get('window');
-let widthOfMargin = Dimensions.get('window').width*0.75
+const widthOfMargin = Dimensions.get('window').width * 0.75;
 
 const onButtonPress = () => {
   Alert.alert('weibo');
-}
+};
 
-export default class ImageButton extends Component{
-  render(){
-    return(
-      <TouchableOpacity  onPress={onButtonPress}  activeOpacity={0.2} focusedOpacity={0.5}>
-         <View style=  {{justifyContent:'center',alignItems:'center',backgroundColor:'#FFFFFF'}}>
-          <Image source={require('./image/weibo.png')} style={{resizeMode:'contain',width:widthOfMargin}}>
-          </Image>
-         </View>
+export default class ImageButton extends Component {
+  render() {
+    return (
+      <TouchableOpacity onPress={onButtonPress} activeOpacity={0.2} focusedOpacity={0.5}>
+        <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFFFFF' }}>
+          <Image
+            source={require('./image/weibo.png')}
+            style={{ resizeMode: 'contain', width: widthOfMargin }} />
+        </View>
       </TouchableOpacity>
-  );
+    );
   }
 }
-
-// export default class WeiboButton extends Component {
-//   constructor(props) {
-//    super(props);
-//    this.state = {text: ''};
-//   }
-//   render() {
-//     return (
-//       <TouchableOpacity onPress={this.props.onPressCallback} style={LoginStyles.weiboTextView}>
-//         <Text style={LoginStyles.loginText} >
-//             {this.props.name}
-//         </Text>
-//       </TouchableOpacity>
-//     );
-//   }
-// }
-
-
-
-// const LoginStyles = StyleSheet.create({
-
-//   loginText: {
-//     color: '#ffffff',
-//     fontWeight: 'bold',
-//     fontSize:20,
-//     width:70,
-//   },
-//   weiboTextView: {
-//     marginTop: 10,
-//     backgroundColor:'red',
-//     height:60,
-//     borderRadius:30,
-//     flexDirection: 'row',
-//     justifyContent: 'center',
-//     width:widthOfMargin,
-//     alignItems:'center',
-//   },
-// });
