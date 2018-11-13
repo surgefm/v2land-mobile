35eab1f2e08081149888b4eaaf88411a7a9f6d7d
IncredibLink <simon.zehua.li@gmail.com>
2018-11-12 23:06:00 -0500

    部分实现事件页面

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { HeaderImage } from './elements';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import { Text } from 'react-native-elements';
+import EventTitle from './EventTitle';
+import { HeaderImage, Subtitle } from './elements';
+import { paddings, paddingConstants } from '../styles';
 
 const Article = ({ event }) => (
-  <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-    <HeaderImage event={event} />
+  <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }} forceInset={{ top: 'never' }}>
+    <ScrollView>
+      <HeaderImage headerImage={event.headerImage} />
+      <View style={[paddings.side, paddings.largeInterval]}>
+        <Subtitle style={[paddings.interval, styles.eventTime]}>今天</Subtitle>
+        <EventTitle style={paddings.interval}>{event.name}</EventTitle>
+        <Text style={styles.eventDescription}>{event.description}</Text>
+      </View>
+      <View style={[paddings.side, paddings.largeInterval]}>
+        <EventTitle style={paddings.interval}>进展</EventTitle>
+        {/* <Subtitle>{event.stack[0].title}</Subtitle> */}
+      </View>
+    </ScrollView>
   </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  eventTime: {
+    paddingTop: paddingConstants.largeInterval,
+  },
+  eventDescription: {
+    fontSize: 14,
+    lineHeight: 18,
+  },
+});
 
 export default Article;
