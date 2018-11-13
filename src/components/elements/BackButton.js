9439eed321493ee524677998993d1074066a96a6
IncredibLink <simon.zehua.li@gmail.com>
2018-11-12 23:05:21 -0500

    实现导航栏“返回上一页面”按钮

---
+++ src/components/elements/BackButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+const BackButton = ({ onPress }) => (
+  <View
+    style={{
+      padding: 6,
+      marginLeft: 10,
+      borderRadius: 100,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(255, 255, 255, .6)',
+    }}>
+    <Icon
+      name={'chevron-left'}
+      onPress={ onPress } />
+  </View>
+);
+
+export default BackButton;
