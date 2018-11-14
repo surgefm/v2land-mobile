a88abca364d9b72e9a9e9255fe14f5b7355c4997
IncredibLink <simon.zehua.li@gmail.com>
2018-11-13 23:49:37 -0500

    Implement getTimeString() method

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,9 @@
 import getAssetURL from './getAssetURL';
+import getTimeString from './getTimeString';
 import getTimeLapseString from './getTimeLapseString';
 
 export {
   getAssetURL,
+  getTimeString,
   getTimeLapseString,
 };
