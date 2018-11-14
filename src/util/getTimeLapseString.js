fdb189e1f228ebb5d0dc91f7413dcd3940f33bb7
IncredibLink <simon.zehua.li@gmail.com>
2018-11-13 23:49:47 -0500

    Improve getTimeLapseString() method

--- src/util/getTimeLapseString.js
+++ src/util/getTimeLapseString.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 
-const getTimeLapseString = (time) => {
+/**
+ * @param {Date|String|Number} time 时间
+ * @param {String} type 输出类型 ['specific', 'general']
+ */
+const getTimeLapseString = (time, type='specific') => {
   if (!time) return;
   if (!_.isDate(time)) {
     try {
@@ -15,14 +19,24 @@ const getTimeLapseString = (time) => {
   timestamp = Math.floor(time.getTime() / 1000);
   const secondDiff = nowTimestamp - timestamp;
   if (secondDiff < 0) return '未来';
-  if (secondDiff < 60) return '刚刚';
-  if (secondDiff < 3600) return `${Math.floor(secondDiff / 60)} 分钟前`;
+  if (type !== 'general') {
+    if (secondDiff < 60) return '刚刚';
+    if (secondDiff < 3600) return `${Math.floor(secondDiff / 60)} 分钟前`;
+  }
 
   const dayDiff = now.getDate() - time.getDate();
   if (dayDiff === 0) return '今天';
-  if (secondDiff < 3600 * 24) return `${Math.floor(secondDiff / 3600)} 小时前`;
+  if (type !== 'general' && secondDiff < 3600 * 24) return `${Math.floor(secondDiff / 3600)} 小时前`;
   if (dayDiff === 1) return '昨天';
-  return `${dayDiff} 天前`;
+  if (type !== 'general') return `${dayDiff} 天前`;
+
+  if (dayDiff <= 3) return '前天';
+  if (dayDiff <= 7) return '三天前';
+  if (dayDiff <= 31) return '一周前';
+  if (dayDiff <= 92) return '一个月前';
+  if (dayDiff <= 183) return '三个月前';
+  if (dayDiff <= 365) return '半年前';
+  return `${Math.floor(dayDiff / 365)} 年前`;
 };
 
 export default getTimeLapseString;
