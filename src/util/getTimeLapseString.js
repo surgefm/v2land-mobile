bdbb0f9a2e795be8d42a284e463af4c460bd826e
IncredibLink <simon.zehua.li@gmail.com>
2018-11-15 00:05:23 -0500

    Fix time lapse calculation

--- src/util/getTimeLapseString.js
+++ src/util/getTimeLapseString.js
@@ -24,7 +24,9 @@ const getTimeLapseString = (time, type='specific') => {
     if (secondDiff < 3600) return `${Math.floor(secondDiff / 60)} 分钟前`;
   }
 
-  const dayDiff = now.getDate() - time.getDate();
+  clearHour(now);
+  clearHour(time);
+  const dayDiff = Math.floor((getSecond(now) - getSecond(time)) / 60 / 60 / 24);
   if (dayDiff === 0) return '今天';
   if (type !== 'general' && secondDiff < 3600 * 24) return `${Math.floor(secondDiff / 3600)} 小时前`;
   if (dayDiff === 1) return '昨天';
@@ -38,5 +40,16 @@ const getTimeLapseString = (time, type='specific') => {
   if (dayDiff <= 365) return '半年前';
   return `${Math.floor(dayDiff / 365)} 年前`;
 };
+
+function clearHour(time) {
+  time.setHours(0);
+  time.setMinutes(0);
+  time.setSeconds(0);
+  time.setMilliseconds(0);
+}
+
+function getSecond(time) {
+  return Math.floor(time.getTime() / 1000);
+}
 
 export default getTimeLapseString;
