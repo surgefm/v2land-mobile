3aa92099d98624124fe8dfc3b7894ccc9263c621
IncredibLink <simon.zehua.li@gmail.com>
2018-11-14 19:28:45 -0500

    Implement setProp enhancer

---
+++ src/enhancers/setProp.js
@@ -0,0 +1,21 @@
+import { createFactory } from 'react';
+import _ from 'lodash';
+
+const setProp = (key, value) => BaseComponent => {
+  const factory = createFactory(BaseComponent);
+  const WithProp = props => {
+    if (_.isFunction(key) && _.isUndefined(value)) {
+      const actual = key(props);
+      key = actual.key;
+      value = actual.value;
+    }
+
+    return factory({
+      ...props,
+      [key]: value,
+    });
+  };
+  return WithProp;
+};
+
+export default setProp;
