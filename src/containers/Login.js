cb4f2cfb679e8a253b0bb042cf96830457bb7cb4
IncredibLink <simon.zehua.li@gmail.com>
2018-11-17 02:36:35 -0500

    Remove redundant enhancers

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,14 +3,12 @@ import LoginComponent from '../components/Login.js';
 
 import {
   withNavigationOptions,
-  prepare,
 } from '../enhancers';
 
 const Article = R.compose(
   withNavigationOptions({
     header: null,
   }),
-  prepare(),
 )(LoginComponent);
 
 export default Article;
