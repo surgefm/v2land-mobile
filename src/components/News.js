a79347776237bb2bda6f95fcaea5dd9c73213209
IncredibLink <simon.zehua.li@gmail.com>
2018-11-18 16:53:43 -0500

    Initialize News Container

---
+++ src/components/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native-elements';
+
+export default class News extends Component {
+  constructor(props) {
+    super(props);
+    _this = this;
+  }
+
+  render() {
+    return (
+      <Text>Under Construction</Text>
+    );
+  }
+}
---
+++ src/containers/News.js
@@ -0,0 +1,5 @@
+import NewsComponent from '../components/News.js';
+
+const News = NewsComponent;
+
+export default News;
