1fff911e1c5902585f1bc361a276562221b9c63e
IncredibLink <link@langchao.org>
2018-11-22 22:05:33 -0500

    Adapt getNewsURL util to the legacy API

--- src/util/getNewsURL.js
+++ src/util/getNewsURL.js
@@ -1,7 +1,17 @@
 import config from '../config/const';
 
 const getNewsURL = (news) => {
-  return `${config.site}${news.eventId}/${news.stackId}/${news.id}`;
+  let eventId = news.eventId;
+  if (!eventId) {
+    eventId = typeof news.event === 'number' ? news.event : news.event.id;
+  }
+
+  let stackId = news.eventId;
+  if (!stackId) {
+    stackId = typeof news.stack === 'number' ? news.stack : news.stack.id;
+  }
+
+  return `${config.site}${eventId}/${stackId}/${news.id}`;
 };
 
 export default getNewsURL;
