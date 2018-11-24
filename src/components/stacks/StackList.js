e3465fcfde40f2debcf4171dc4e7c350384ae1c7
IncredibLink <link@langchao.org>
2018-11-24 04:55:51 -0500

    Update NewsItem and StackItem

--- src/components/stacks/StackList.js
+++ src/components/stacks/StackList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FlatList } from 'react-native';
-import StackItem from './StackItem';
+import StackItem from '../../containers/StackItem';
 
 const StackList = ({ stacks, onPress, onNewsPress }) => !stacks || (
   <FlatList
     data={stacks}
     renderItem={({ item }) => <StackItem
-      stack={item}
-      isLastStack={item.id === stacks[stacks.length - 1].id}
+      stackId={item}
+      isLastStack={item === stacks[stacks.length - 1]}
       onPress={onPress}
       onNewsPress={onNewsPress} /> }
     keyExtractor={stack => `Stack#${stack.id}`}
---
+++ src/containers/NewsItem.js
@@ -0,0 +1,12 @@
+import R from 'ramda';
+import NewsItemComponent from '../components/news/NewsItem';
+import { connect } from '../enhancers';
+import { newsSelector } from '../store/selectors/news';
+
+const NewsItem = R.compose(
+  connect({
+    news: newsSelector,
+  }),
+)(NewsItemComponent);
+
+export default NewsItem;
