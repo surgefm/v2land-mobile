a72a46b7fab624a15962f14f9ab661aa1ad27152
IncredibLink <link@langchao.org>
2019-02-04 14:04:16 -0500

    Use react-native-picker-select instead of buttongroup for subscription methods

--- src/components/SubscriptionEditor.js
+++ src/components/SubscriptionEditor.js
@@ -6,8 +6,27 @@ export default class SubscriptionEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      method: 0,
-      mode: 3,
+      method: 'appNotification',
+      methods: [{
+        label: 'App 通知',
+        value: 'appNotification',
+      }, {
+        label: '@ 你的 Twitter 账号',
+        value: 'twitterAt',
+      }, {
+        label: '使用你的 Twitter 账号发推',
+        value: 'twitter',
+      }, {
+        label: '@ 你的微博账号',
+        value: 'weiboAt',
+      }, {
+        label: '使用你的微博账号发贴',
+        value: 'weibo',
+      }, {
+        label: '邮件通知',
+        value: 'email',
+      }],
+      mode: 2,
       modeLabel: '每当有新的进展时提醒我',
     };
   }
@@ -16,13 +35,15 @@ export default class SubscriptionEditor extends Component {
     this.alert = alert;
   }
 
+  updateMethod(method) {
+    this.setState({ ...this.state, method });
+  }
+
   updateMode(mode) {
     const modeNames = [
-      '在事件有一年没有新消息时提醒我',
       '在事件有 30 天没有新消息时提醒我',
       '每周五提醒我',
       '每当有新的进展时提醒我',
-      '每当有新的新闻时提醒我',
     ];
 
     this.setState({
@@ -43,6 +64,7 @@ export default class SubscriptionEditor extends Component {
         {alert => SubscriptionEditorComponent({
           ...this.props,
           ...this.state,
+          updateMethod: this.updateMethod.bind(this),
           updateMode: this.updateMode.bind(this),
           submitSubscriptionRequest: this.submitSubscriptionRequest.bind(this),
           setAlert: this.setAlert(alert),
