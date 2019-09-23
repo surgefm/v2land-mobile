66920a75c2bbfce3d50d29bd65858068fb47ccb6
Peking Roast Duck <link@langchao.org>
2019-09-22 21:31:59 -0400

    Update Travis configuration (#48)
    * Update .travis.yml
    
    * Update pod repo
    
    * Use brew to install Ruby
    
    * Add notification support
    
    * Update test cases.

---
+++ test/store/transducers/fallback.test.js
@@ -0,0 +1,20 @@
+import fallback from '../../../src/store/transducers/fallback.js';
+
+describe('fallback', () => {
+  let initialState;
+  let fallbackReducer;
+
+  beforeEach(() => {
+    initialState = {};
+    fallbackReducer = fallback(initialState);
+  });
+
+  it('should return initialState by default', () => {
+    expect(fallbackReducer()).toBe(initialState);
+  });
+
+  it('should return the current state', () => {
+    const currentState = {x: 1};
+    expect(fallbackReducer(currentState)).toBe(currentState);
+  });
+});
